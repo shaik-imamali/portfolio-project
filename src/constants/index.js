9f6b1825d3b03da100092288e15213bde8bb9626
shaik iamali <162468441+shaik-imamali@users.noreply.github.com>
2025-03-23 12:15:50 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,69 +14,69 @@ export const HERO_CONTENT = `I am an enthusiastic and driven full-stack develope
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const PROJECTS = [
-  {
-    title: "Rock-Paper-Scissors",
-    image: project1,
-    description:
-      "A funny Web app game Played by Computer Vs You. Hosted Public can be used by everyone. Link: https://manideep-prs.netlify.app",
-    technologies: ["HTML", "CSS", "JavaScript"],
-  },
-  {
-    title: "Tic-Tac-Toe",
-    image: project2,
-    description:
-      "Simple fun and friendly Tic-Tac-Toe Game App for Multiplayer. Hosted public and availabe for everyone. Using Netlify. Link: https://manideep-tic-tac-toe.netlify.app",
-    technologies: ["HTML", "CSS", "JavaScript"],
-  },
-  {
-    title: "React Weather App",
-    image: project3,
-    description:
-      "A Effective Weather App made using Weather API to fetch data of specific location.Hosted the site in public using Netlify. Link: https://manideep-weather-app.netlify.app",
-    technologies: ["HTML", "CSS", "React", "tailwindcss"],
-  },
-  {
-    title: "Simple Calculator",
-    image: project4,
-    description:
-      "A simple Basic Calculator builded using React. Not Hosted by code is available in the github",
-    technologies: ["HTML", "CSS", "React"],
-  },
-  {
-    title: "Simple BMI Calculator",
-    image: project5,
-    description:
-      "A simple BMI Calculator builded using React. Used to perform all the BMI operations. Not Hosted by code is available in the github",
-    technologies: ["HTML", "CSS", "React"],
-  },
-  {
-    title: "Groceries List App",
-    image: project6,
-    description:
-      "A Simple ToDo List builded using React used to add the Items and Edit or delete them. Not Hosted by code is available in the github",
-    technologies: ["HTML", "CSS", "React"],
-  },
-  {
-    title: "JWT Authentication Demo",
-    image: project7,
-    description:
-      "A Sample Login and SignUp page which perform authentication. Shows the working of webTokens. Code is available in the github.",
-    technologies: ["HTML", "CSS", "React", "Express", "Mongodb", "NodeJS"],
-  },
+  // {
+  //   title: "Rock-Paper-Scissors",
+  //   image: project1,
+  //   description:
+  //     "A funny Web app game Played by Computer Vs You. Hosted Public can be used by everyone. Link: https://manideep-prs.netlify.app",
+  //   technologies: ["HTML", "CSS", "JavaScript"],
+  // },
+  // {
+  //   title: "Tic-Tac-Toe",
+  //   image: project2,
+  //   description:
+  //     "Simple fun and friendly Tic-Tac-Toe Game App for Multiplayer. Hosted public and availabe for everyone. Using Netlify. Link: https://manideep-tic-tac-toe.netlify.app",
+  //   technologies: ["HTML", "CSS", "JavaScript"],
+  // },
+  // {
+  //   title: "React Weather App",
+  //   image: project3,
+  //   description:
+  //     "A Effective Weather App made using Weather API to fetch data of specific location.Hosted the site in public using Netlify. Link: https://manideep-weather-app.netlify.app",
+  //   technologies: ["HTML", "CSS", "React", "tailwindcss"],
+  // },
+  // {
+  //   title: "Simple Calculator",
+  //   image: project4,
+  //   description:
+  //     "A simple Basic Calculator builded using React. Not Hosted by code is available in the github",
+  //   technologies: ["HTML", "CSS", "React"],
+  // },
+  // {
+  //   title: "Simple BMI Calculator",
+  //   image: project5,
+  //   description:
+  //     "A simple BMI Calculator builded using React. Used to perform all the BMI operations. Not Hosted by code is available in the github",
+  //   technologies: ["HTML", "CSS", "React"],
+  // },
+  // {
+  //   title: "Groceries List App",
+  //   image: project6,
+  //   description:
+  //     "A Simple ToDo List builded using React used to add the Items and Edit or delete them. Not Hosted by code is available in the github",
+  //   technologies: ["HTML", "CSS", "React"],
+  // },
+  // {
+  //   title: "JWT Authentication Demo",
+  //   image: project7,
+  //   description:
+  //     "A Sample Login and SignUp page which perform authentication. Shows the working of webTokens. Code is available in the github.",
+  //   technologies: ["HTML", "CSS", "React", "Express", "Mongodb", "NodeJS"],
+  // },
   {
     title: "Student CRUD App",
     image: project8,
     description:
-      "Application which perform all the CRUD operations. Regidter, Search, Update, and Delete Student Details.Mongo CLoud is used as database Storage.Hosted the website using Netlify. Link: https://manideep-crud-app.netlify.app",
+      "Application which perform all the CRUD operations. Regidter, Search, Update, and Delete Student Details.Mongo CLoud is used as database Storage.",
     technologies: ["HTML", "CSS", "React", "Express", "Mongodb", "NodeJS"],
   },
-  {
-    title: "Student Portfolio",
-    image: project9,
-    description:
-      "A Attractive Portfolo describing my Technlogies and Projects.Also having Feature to contact Me using viewr-details.Hosted the website using Netlify. Link: https://pm-portfolio-site.netlify.app",
-    technologies: ["HTML", "CSS", "React", "Express", "Mongodb", "NodeJS"],
-  },
+  // {
+  //   title: "Student Portfolio",
+  //   image: project9,
+  //   description:
+  //     "A Attractive Portfolo describing my Technlogies and Projects.Also having Feature to contact Me using viewr-details.Hosted the website using Netlify. Link: https://pm-portfolio-site.netlify.app",
+  //   technologies: ["HTML", "CSS", "React", "Express", "Mongodb", "NodeJS"],
+  // },
 ];
 
 export const CONTACT = {
